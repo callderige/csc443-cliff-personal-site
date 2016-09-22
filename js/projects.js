91de17148f13fa39d16cb7fddc8e38dc82145046
Cliff Allderige <ca.allderige@gmail.com>
2016-09-22 08:51:02 -0400

    Refactor amazon bulk search.

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,12 @@
-$("a.amazon-search-btn").on("click",function(){
-    var search = document.getElementById('amazon_search').value;
-    if(search.length > 0){
-	    var str = search.split(",");
-	    var str_len = str.length;
-	    for(var i = 0; i < str_len; i++){
-	        window.open('https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+str[i], '_blank');
+$("a.amazon-search-btn").on("click",function() {
+    let search = document.getElementById('amazon_search').value;
+
+    if(search.length > 0) {
+	    let searchedItemsArray = search.split(",");
+	    for(let i = 0; i < searchedItemsArray.length; i++) {
+	        window.open('https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords='+searchedItemsArray[i], '_blank');
 	    }
+	} else {
+		window.alert("Search field is blank, please enter one or more items.")
 	}
 });
